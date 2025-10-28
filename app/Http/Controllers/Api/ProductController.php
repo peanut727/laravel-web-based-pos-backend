ef0adf27d420c5437d2e95b378aa85faecbe336a
peanut <juntinpao27@gmail.com>
2025-10-28 19:06:22 +0800

    feat: add barcode generation helper and refactored ProductController

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Helpers\BarcodeHelper;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
@@ -12,26 +13,43 @@ class ProductController extends Controller
     // GET
     public function index()
     {
-        return response()->json([Product::all()]);
+        return response()->json(Product::all());
     }
 
     // POST
     public function store(Request $request)
     {
-        $product = Product::create($request->all());
-        return response()->json($product, 201);
+        $validated = $request->validate([
+            'name' => 'required|string|unique:products,name',
+            'category' => 'required|string',
+            'price'=> 'required|numeric',
+            'stock' => 'required|numeric',
+            'barcode'=> 'nullable|string|unique:products,barcode',
+        ]);
+
+        if (empty($validated['barcode'])) {
+            $validated['barcode'] = BarcodeHelper::generateBarcode();
+        }
+
+        $product = Product::create($validated);
+         return response()->json([
+            'message' => 'Product created successfully!',
+            'data' => $product
+         ],  201);
     }
 
     // GET::ID
     public function show(string $id)
     {
-        return response()->json([Product::findOrFail($id)]);
+        return response()->json(Product::findOrFail($id));
     }
 
     // PUT
     public function update(Request $request, string $id)
     {
-        
+        $product = Product::findorFail($id);
+        $product->update($request->all());
+        return response()->json($product, 204);
     }
 
     // DELETE
